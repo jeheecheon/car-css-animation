5f90bfaf4e4d87c0c2f9d173917a692363ace2f6
Jehee Cheon <jeheecheon@gmail.com>
2024-11-13 15:27:35 +0900

    chore: 전역 스타일 리셋을 위한 GlobalStyles 컴포넌트 추가
    기본적인 전역 스타일 리셋을 적용하기 위한 GlobalStyles 컴포넌트 추가

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  :root, html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyles;
